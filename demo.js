2893a70698322eee3c03662a47c2dd89874716ec
indhu-98 <ilamathiadu@gmail.com>
2024-06-10 16:12:23 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,9 +1,6 @@
-var birthday = true
-if(birthday)
-{
-console.log("happy birthday")
-}
-else
-{
-    console.log("have a greatday")
-}
+console.log(false && true && true)
+console.log(true && true && true)
+console.log(false && false && false)
+console.log(true || false || false)
+console.log(!true)
+console.log(!false)
