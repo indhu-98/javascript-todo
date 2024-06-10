8d27c6e5775c692b092f6d184eed9e3950afae57
indhu-98 <ilamathiadu@gmail.com>
2024-06-10 16:19:44 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,13 @@
-console.log(false && true && true)
-console.log(true && true && true)
-console.log(false && false && false)
-console.log(true || false || false)
-console.log(!true)
-console.log(!false)
+var color = "green"
+if(color == "red")
+    {
+    console.log("stop")
+}
+if(color == "yellow")
+{
+    console.log("ready to go")
+}
+if(color == "green")
+    {
+        console.log("go")
+    }
