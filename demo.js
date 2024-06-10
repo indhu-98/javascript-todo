d65e0f927bd85206830e13d8b84fa454d4891627
indhu-98 <ilamathiadu@gmail.com>
2024-06-10 16:46:52 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,18 +1,10 @@
-var season = "winter"
-if(season == "spring")
-    {
-    console.log("enjoy the blooming flowers")
-    }
-    if(season == "summer")
-        {
-            console.log("have fun in sun")
-        }
-        if(season == "autumn")
-            {
-                console.log("admire the colorful leaves")
-            }
-            if(season == "winter")
-                {
-                    console.log("bundle up and stay warm")
-                }
-
+var score = 49
+if (score <50) {
+    console.log("u need to improve")
+}
+if (score > 50 || score >70) {
+    console.log("good job")
+}
+if (score > 70) {
+    console.log("excellent performance")
+}
